3f640c0355500a94247223be03ed3f2b9258e2bb
irzanurf <irzadexter@gmail.com>
2021-03-04 20:05:36 +0700

    bug rev penelitian

--- application/views/reviewer/penelitian/penilaian.php
+++ application/views/reviewer/penelitian/penilaian.php
@@ -25,8 +25,7 @@
                 <td align="center"><?= $v->jenis ?></td>
                 <td align="center"><?= $v->nilai ?></td>
                 <td align="center">
-                <?php if($v->status==11 || $v->status == 13 ||$v->status == 2) : ?>
-                <?php elseif($v->nilai!=NULL) : ?>
+                <?php if($v->status==11 || $v->status == 13 ||$v->status == 2 || $v->nilai!=NULL) : ?>
                     <form style="display:inline-block;" method="post" action=<?= base_url('reviewer/penelitian/editPenilaian');?>>
                                     <input type='hidden' name="id" value="<?= $v->id ?>">
                                     <input type='hidden' name="jenis" value="<?= $v->id_jenis ?>">
@@ -65,14 +64,14 @@
                 <td align="center"><?= $v->jenis?></td>
                 <td align="center"><?= $v->nilai2 ?></td>
                 <td align="center">
-                <?php if($v->status==12 || $v->status == 13 ||$v->status == 2) : ?>
-                <?php elseif($v->nilai2!=NULL) : ?>
+                <?php if($v->status==12 || $v->status == 13 ||$v->status == 2 || $v->nilai2!=NULL) : ?>
                     <form style="display:inline-block;" method="post" action=<?= base_url('reviewer/penelitian/editPenilaian');?>>
                                     <input type='hidden' name="id" value="<?= $v->id ?>">
                                     <input type='hidden' name="jenis" value="<?= $v->id_jenis ?>">
                                     <button type="submit" class="btn-sm btn-primary">
                                         Edit
                                     </button>
+                
                                  
                 <?php elseif($v->status==0 || $v->status==1 || $v->nilai2==NULL ||$v->nilai2=="" ||$v->nilai2=="0") :?>
                     <form method="post" action=<?= base_url('reviewer/penelitian/formPenilaian');?>>
