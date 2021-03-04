8bf1702349b99584f6f7e58bb305056b441af0e6
irzanurf <irzadexter@gmail.com>
2021-03-04 20:28:46 +0700

    bug rev penl

--- application/views/reviewer/penelitian/penilaian.php
+++ application/views/reviewer/penelitian/penilaian.php
@@ -25,7 +25,17 @@
                 <td align="center"><?= $v->jenis ?></td>
                 <td align="center"><?= $v->nilai ?></td>
                 <td align="center">
-                <?php if($v->nilai!=NULL ||$v->nilai!="" ||$v->nilai!="0") : ?>
+
+                <?php if($v->nilai==NULL ||$v->nilai=="" ||$v->nilai=="0") :?>
+                    <form method="post" action=<?= base_url('reviewer/penelitian/formPenilaian');?>>
+                                    <input type='hidden' name="id" value="<?= $v->id ?>">
+                                    <input type='hidden' name="jenis" value="<?= $v->id_jenis ?>">
+                                    <button type="submit" class="btn-sm btn-primary">
+                                        Penilaian
+                                    </button>
+                                    
+                                </form>
+                <?php elseif($v->nilai!=NULL ||$v->nilai!="" ||$v->nilai!="0") : ?>
                     <form style="display:inline-block;" method="post" action=<?= base_url('reviewer/penelitian/editPenilaian');?>>
                                     <input type='hidden' name="id" value="<?= $v->id ?>">
                                     <input type='hidden' name="jenis" value="<?= $v->id_jenis ?>">
@@ -35,15 +45,7 @@
                                     
                                     
                                 </form>
-                <?php elseif($v->nilai==NULL ||$v->nilai=="" ||$v->nilai=="0") :?>
-                    <form method="post" action=<?= base_url('reviewer/penelitian/formPenilaian');?>>
-                                    <input type='hidden' name="id" value="<?= $v->id ?>">
-                                    <input type='hidden' name="jenis" value="<?= $v->id_jenis ?>">
-                                    <button type="submit" class="btn-sm btn-primary">
-                                        Penilaian
-                                    </button>
-                                    
-                                </form>
+                
 
                 <?php elseif($v->status==2 || $v->status==3 || $v->status==4 || $v->status==5) : ?>
                                 <button type="button" class="btn-sm btn-default" dissabled>
@@ -64,16 +66,8 @@
                 <td align="center"><?= $v->jenis?></td>
                 <td align="center"><?= $v->nilai2 ?></td>
                 <td align="center">
-                <?php if($v->nilai2!=NULL ||$v->nilai2!="" ||$v->nilai2!="0") : ?>
-                    <form style="display:inline-block;" method="post" action=<?= base_url('reviewer/penelitian/editPenilaian');?>>
-                                    <input type='hidden' name="id" value="<?= $v->id ?>">
-                                    <input type='hidden' name="jenis" value="<?= $v->id_jenis ?>">
-                                    <button type="submit" class="btn-sm btn-primary">
-                                        Edit
-                                    </button>
-                
-                                 
-                <?php elseif($v->nilai2==NULL ||$v->nilai2=="" ||$v->nilai2=="0") :?>
+
+                <?php if($v->nilai2==NULL ||$v->nilai2=="" ||$v->nilai2=="0") :?>
                     <form method="post" action=<?= base_url('reviewer/penelitian/formPenilaian');?>>
                                     <input type='hidden' name="id" value="<?= $v->id ?>">
                                     <input type='hidden' name="jenis" value="<?= $v->id_jenis ?>">
@@ -81,7 +75,18 @@
                                         Penilaian
                                     </button>
                                     
+                                </form> 
+                <?php elseif($v->nilai2!=NULL ||$v->nilai2!="" ||$v->nilai2!="0") : ?>
+                    <form style="display:inline-block;" method="post" action=<?= base_url('reviewer/penelitian/editPenilaian');?>>
+                                    <input type='hidden' name="id" value="<?= $v->id ?>">
+                                    <input type='hidden' name="jenis" value="<?= $v->id_jenis ?>">
+                                    <button type="submit" class="btn-sm btn-primary">
+                                        Edit
+                                    </button>
+                                    
+                                    
                                 </form>
+                
                 
                 <?php elseif($v->status==2 || $v->status==3 || $v->status==4 || $v->status==5) : ?>
                                 <button type="button" class="btn-sm btn-default" dissabled>
